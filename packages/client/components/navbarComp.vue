2ce6001cd0e242a472148fcae499cca6c8257145
Wesley Ford <wesleyford@ntohq.dev>
2022-05-03 03:25:31 -0500

    Navbar Finished

---
+++ packages/client/components/navbarComp.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-navbar
+    class="navbar header has-shadow is-primary"
+    role="navigation"
+    :mobile-burger="true"
+    aria-label="main navigation"
+  >
+    <template #brand>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }" class="mx-4">
+        <img src="~assets/header-logo.png" alt="MVM Designer" height="28" />
+      </b-navbar-item>
+    </template>
+    <template  #end>
+      <b-navbar-item v-for="{ link, name } in nav" :key="name" tag="div">
+        <NuxtLink :to="{ path: link }" class="is-size-4 navbar-item" exact-active-class="is-active">
+          {{ name }}
+        </NuxtLink>
+      </b-navbar-item>
+      <hr class="navbar-divider">
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nav: [
+        {
+          name: 'Home',
+          link: '/',
+        },
+        {
+          name: 'About Me',
+          link: '#about-me',
+        },
+        {
+          name: 'Portfolio',
+          link: '/portfolio',
+        },
+        {
+          name: 'Contact Me',
+          link: '/contact-me',
+        },
+      ],
+    }
+  },
+}
+</script>
