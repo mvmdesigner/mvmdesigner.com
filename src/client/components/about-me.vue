43f8db653b98c6b35e59b51dc2e7bac0ca14367e
Wesley Ford <wes@wesdev.pro>
2023-03-01 15:45:09 -0600

    Updated Hero Component
    1. Changed background image
    2. Removed old images

--- src/client/components/about-me.vue
+++ src/client/components/about-me.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="hero about-hero box">
+  <section id="about-me" class="hero about-hero box">
     <div class="hero-content">
       <div class="hero-img">
         <img src="~assets/michealprofilenum2.jpg" alt="A portrait of Michael Mercadante" max-height="10px" />
@@ -16,42 +16,45 @@
 
 <style lang="scss" scoped>
 .about-hero {
-  background: url('@/assets/abstract_art.jpg')center center; background-size:cover; 
-    min-height: 500px;
-    background-attachment: fixed;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    
-    @media (max-width: calc(960px + (2 * 0.75rem))) {
-        background-attachment: inherit;
-    }
-    .hero-content {
-      display: grid;
-      grid-template-rows: 2fr 1fr;
+  background: url('@/assets/blue-shapes.jpg') center center;
+  background-size: cover;
+  min-height: 500px;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  @media (max-width: calc(960px + (2 * 0.75rem))) {
+    background-attachment: inherit;
+  }
+
+  .hero-content {
+    display: grid;
+    grid-template-rows: 2fr 1fr;
+    margin: auto;
+    padding: 2.5rem;
+
+    .hero-img {
       margin: auto;
-      padding: 2.5rem;
 
-      .hero-img {
+      img {
+        border-radius: 50%;
         margin: auto;
-        img {
-          border-radius: 50%;
-          margin: auto;
-          max-height: 8rem;
-        }
-      }
-
-      .hero-text {
-        h4 {
-          text-align: center;
-          color: white;
-        }
-
-        p {
-          color: white;
-          font-weight: 500;
-        }
+        max-height: 8rem;
       }
     }
+
+    .hero-text {
+      h4 {
+        text-align: center;
+        color: white;
+      }
+
+      p {
+        color: white;
+        font-weight: 500;
+      }
+    }
+  }
 }
 </style>
