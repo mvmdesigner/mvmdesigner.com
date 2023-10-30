81ab3a5d1a73181102739e61f2b8f8442c8fb9f4
Wesley Ford <wes@wesdevpro.com>
2023-10-30 01:07:39 -0500

    chore: added originial colors to `tailwindcss` config

--- src/client/tailwindcss.config.mjs
+++ src/client/tailwindcss.config.mjs
@@ -2,6 +2,12 @@
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
+      colors: {
+        'primary': '#053255',
+        'primary-invert': findColorInvert('#053255'),
+        'gold': '#bd8f21',
+        'gold-invert': findColorInvert('#bd8f21'),
+      },
       extend: {},
     },
     plugins: [],
